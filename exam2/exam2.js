ca7cca59b05060fe8c3eaee31397c6a45d39aff5
daniel-west <daniel.west.2014@gmail.com>
2015-04-24 14:25:02 -0400

    Update exam2.js

--- exam2/exam2.js
+++ exam2/exam2.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 	$.getJSON("https://hivelab.org/static/exam2.json", function (json) {
         var tr;
+        var score;
         for (var i = 0; i < json.length; i++) {
             tr = $('<tr/>');
             tr.append("<td>" + json[i].ItemNo + "</td>");
